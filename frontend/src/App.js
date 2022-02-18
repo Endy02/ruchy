cc1faf1f8b550410dd9b52bb1953b247464db873
fujyn <endy.alexis@outlook.com>
2022-02-18 19:21:53 +0100

    Create React navbar
    Added:
    - Button component
    - Initial homepage setup
    
    Updated:
    - Navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './layers/navbar'
+import Footer from './layers/footer'
+import {Home} from './pages/Home'
 
 class App extends Component {
     render() {
         return (
             <>
-                <h1 className='big-text black-text pad-l-m pad-t-m'>React is active man</h1>
+                <Router>
+                    <Navbar />
+                    <Routes>
+                        <Route exact path="/" element={<Home />} />
+                        <Route exact path="/statistics" element="" />
+                        <Route exact path="/simulator" element="" />
+                        <Route exact path="/predictions" element="" />
+                        <Route exact path="/profile" element="" />
+                        <Route exact path="/settings" element="" />
+                    </Routes>
+                    <Footer />
+                </Router>
             </>
         )
     }
