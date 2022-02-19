b8eb43ae25060a5c10d64ce2e6e9ecc533dcbd60
fujyn <endy.alexis@outlook.com>
2022-02-19 14:47:11 +0100

    Update Global app settings
    Updated:
    - Global app settings
    - Connection component initialization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './layers/navbar'
-import Footer from './layers/footer'
-import {Home} from './pages/Home'
-
+import Navbar from './layers/Navbar'
+import Footer from './layers/Footer'
+import Home from './pages/Home'
+import Statistics from './pages/statistics/Statistics'
+import Simulator from './pages/simulator/Simulator'
+import Predictions from './pages/predictions/Predictions'
+import Profile from './pages/users/Profile'
+import Settings from './pages/users/Settings'
 class App extends Component {
     render() {
         return (
@@ -13,11 +17,11 @@ class App extends Component {
                     <Navbar />
                     <Routes>
                         <Route exact path="/" element={<Home />} />
-                        <Route exact path="/statistics" element="" />
-                        <Route exact path="/simulator" element="" />
-                        <Route exact path="/predictions" element="" />
-                        <Route exact path="/profile" element="" />
-                        <Route exact path="/settings" element="" />
+                        <Route exact path="/statistics" element={<Statistics />} />
+                        <Route exact path="/simulator" element={<Simulator />} />
+                        <Route exact path="/predictions" element={<Predictions />} />
+                        <Route exact path="/profile" element={<Profile />} />
+                        <Route exact path="/settings" element={<Settings />} />
                     </Routes>
                     <Footer />
                 </Router>
