eed5841127d2faa22e9c6c53f6b4f0aaf7995d5c
fujyn <endy.alexis@outlook.com>
2022-03-02 03:16:34 +0100

    Initialize statistics component
    Added:
    - Home statistic preview base structure

--- frontend/src/pages/statistics/Statistics.js
+++ frontend/src/pages/statistics/Statistics.js
@@ -1,12 +1,39 @@
 import React from 'react'
+import Button from '../../components/Button'
+import Cards from '../../components/Cards'
 
 const Statistics = () => {
   return (
-      <>
-        <div className='container'>
-            <h1 className='big-text bold-text black-text pad-l-m'>Statistics</h1>
+    <>
+      <div className='container'>
+        <div className='container-full-width flex-col-center full-height'>
+          <div className='full-gutter'>
+            <div className='container-grid-2'>
+              <div className='grid-item flex-col-center'>
+                <div className='container-content flex-col-center'>
+                  <div className='card-cover'>
+                    <div className='card-list pad-b-l'>
+                      <div className='card-list-date'>
+                        <p className='medium-text bold-text'>Sunday February 20 2022</p>
+                      </div>
+                      <Cards team_1='chicago-bulls' team_2='boston-celtics' team_1_score="129" team_2_score='112' team_win='chicago-bulls' />
+                      <Cards team_1='new-york-knicks' team_2='los-angeles-lakers' team_1_score='106' team_2_score='114' team_win='los-angeles-lakers' />
+                      <Cards team_1='golden-state' team_2='san-antonio-spurs' team_1_score="130" team_2_score='128' team_win='golden-state' />
+                    </div>
+                  </div>
+                  <Button buttonSize="btn-large" mode="btn-regular" buttonStyle='btn-current' link='/statistics'>Check out</Button>
+                </div>
+              </div>
+              <div className='grid-item flex-col-center mobile'>
+                <div className='card-display'>
+                  <img src='/assets/images/kobe-memorial-wall.jpg' className='rectangle-img' />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </>
+      </div>
+    </>
   )
 }
 
