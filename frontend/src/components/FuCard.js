dd614f575927135706d665b20b94acc5aa2ef336
fujyn <endy.alexis@outlook.com>
2022-03-02 02:52:56 +0100

    Create new card component
    Added:
    - FuCard component
    
    Updated:
    - Cards comonent

---
+++ frontend/src/components/FuCard.js
@@ -0,0 +1,82 @@
+import React from 'react'
+
+const FuCard = () => {
+  return (
+    <>
+      <div className='fu-card-wrapper pad-b-l'>
+        <div className='fu-card'>
+          <div className='fu-card-left fu-card-win-left'>
+            <div className='fu-card-img'>
+              <div className='card-logo-large'>
+                <img className='card-img-logo' src="/assets/images/teams/chicago-bulls.png" />
+              </div>
+            </div>
+            <div className='fu-card-content'>
+              <p className='large-text bold-text pad-l-s'>85 %</p>
+            </div>
+          </div>
+          <div className='fu-card-right'>
+            <div className='fu-card-img'>
+              <div className='card-logo-large'>
+                <img className='card-img-logo' src="/assets/images/teams/boston-celtics.png" />
+              </div>
+            </div>
+            <div className='fu-card-content'>
+              <p className='large-text bold-text pad-r-s'>15 %</p>
+            </div>
+          </div>
+        </div>
+        <div className='fu-card-desc pad-t-m'>
+          <table className='fu-card-table'>
+            <tr className='fu-card-table-line'>
+              <td className='fu-card-table-cell'>
+                <p className='medium-text regular-text'>78 %</p>
+              </td>
+              <td className='fu-card-table-cell'>
+                <p className='medium-text regular-text'>Free throw</p>
+              </td>
+              <td className='fu-card-table-cell'>
+                <p className='medium-text regular-text'>22 %</p>
+              </td>
+            </tr>
+            <tr className='fu-card-table-line'>
+              <td className='fu-card-table-cell'>
+                <p className='medium-text regular-text'>78 %</p>
+              </td>
+              <td className='fu-card-table-cell'>
+                <p className='medium-text regular-text'>Assist</p>
+              </td>
+              <td className='fu-card-table-cell'>
+                <p className='medium-text regular-text'>22 %</p>
+              </td>
+            </tr>
+            <tr className='fu-card-table-line'>
+              <td className='fu-card-table-cell'>
+                <p className='medium-text regular-text'>78 %</p>
+              </td>
+              <td className='fu-card-table-cell'>
+                <p className='medium-text regular-text'>Field goal</p>
+              </td>
+              <td className='fu-card-table-cell'>
+                <p className='medium-text regular-text'>22 %</p>
+              </td>
+            </tr>
+            <tr className='fu-card-table-line'>
+              <td className='fu-card-table-cell'>
+                <p className='medium-text regular-text'>78 %</p>
+              </td>
+              <td className='fu-card-table-cell'>
+                <p className='medium-text regular-text'>Rebonds</p>
+              </td>
+              <td className='fu-card-table-cell'>
+                <p className='medium-text regular-text'>22 %</p>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default FuCard
