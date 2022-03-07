45d12e6d8cb92bffcdbc72df09c7117edb9ea078
fujyn <endy.alexis@outlook.com>
2022-03-07 11:55:20 +0100

    Update front config
    Updated:
    - Webpack config
    - Project main urls

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,25 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      { 
+        test: /\.css$/, 
+        use: 'css-loader'
+      },
+      {
+        test: /\.s[ac]ss$/i, 
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]},
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
   optimization: {
