a919ecf8c4457436a36e3ce61f683622b2b92dea
fujyn <endy.alexis@outlook.com>
2022-03-07 11:55:20 +0100

    Update front config
    Updated:
    - Webpack config
    - Project main urls

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,26 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    # Commun views
+    path('predictions', views.index, name='predictions'),
+    path('prediction/<int:id>', views.index, name='prediction-details'),
+    path('simulator', views.index, name='simulator'),
+    path('simulate/<int:id_t1>/<int:id_t2>', views.index, name='simulation'),
+    path('statistics', views.index, name='statistics'),
+    path('statistics/<int:id>', views.index, name='statistic-details'),
+    path('team/<slug:slug>', views.index, name='team-details'),
+    path('', views.index, name='home'),
+    
+    # Authentication views
+    path('login', views.index, name='login'),
+    path('register', views.index, name='register'),
+    path('forgot-password', views.index, name='forgot-password'),
+    path('confirm-reset-pasword', views.index, name='confirm-reset-password'),
+    path('reset-password', views.index, name='reset-password'),
+    
+    # User views
+    path('profile', views.index, name='profile'),
+    path('settings', views.index, name='settings'),
 ]
