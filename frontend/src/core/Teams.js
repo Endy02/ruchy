9345f58b8b56efd8c0d7286793e6912bdfd78dbf
fujyn <endy.alexis@outlook.com>
2022-03-11 16:20:45 +0100

    Create Team logo mapper
    Added:
    - Team logo mapper (return object logo)

---
+++ frontend/src/core/Teams.js
@@ -0,0 +1,155 @@
+import atlantaHawks from '../../../assets/images/teams/atlanta-hawks.svg'
+import bostonCeltics from '../../../assets/images/teams/boston-celtics.svg';
+import brooklynNets from '../../../assets/images/teams/brooklyn-nets.svg';
+import charlotteHornets from '../../../assets/images/teams/charlotte-hornets.svg';
+import chicagoBulls from '../../../assets/images/teams/chicago-bulls.svg';
+import clevelandCavaliers from '../../../assets/images/teams/cleveland-cavaliers.svg';
+import dallasMavericks from '../../../assets/images/teams/dallas-mavericks.svg';
+import denverNuggets from '../../../assets/images/teams/denver-nuggets.svg';
+import detroitPiston from '../../../assets/images/teams/detroit-pistons.svg';
+import goldenState from '../../../assets/images/teams/golden-state-warriors.svg';
+import houstonRockets from '../../../assets/images/teams/houston-rockets.svg';
+import indianaPacers from '../../../assets/images/teams/indiana-pacers.svg';
+import losAngelesClippers from '../../../assets/images/teams/los-angeles-clippers.svg'
+import losAngelesLakers from '../../../assets/images/teams/los-angeles-lakers.svg';
+import memphisGrizzlies from '../../../assets/images/teams/memphis-grizzlies.svg';
+import miamiHeat from '../../../assets/images/teams/miami-heat.svg';
+import milwaukeeBucks from '../../../assets/images/teams/milwaukee-bucks.svg';
+import minnesotaTimberwolves from '../../../assets/images/teams/minnesota-timberwolves.svg';
+import newOrleansPelicans from '../../../assets/images/teams/new-orleans-pelicans.svg';
+import newYorkKnicks from '../../../assets/images/teams/new-york-knicks.svg';
+import oklahomaCityThunder from '../../../assets/images/teams/oklahoma-city-thunder.svg';
+import orlandoMagic from '../../../assets/images/teams/orlando-magic.svg';
+import philadelphia76ers from '../../../assets/images/teams/philadelphia-76ers.svg';
+import phoenixSuns from '../../../assets/images/teams/phoenix-suns.svg';
+import portlandTrailBlazers from '../../../assets/images/teams/portland-trail-blazers.svg';
+import sacramentoKings from '../../../assets/images/teams/sacramento-kings.svg'
+import sanAntonioSpurs from '../../../assets/images/teams/san-antonio-spurs.svg';
+import torontoRaptors from '../../../assets/images/teams/toronto-raptors.svg'
+import utahJazz from '../../../assets/images/teams/utah-jazz.svg';
+import washingtonWizards from '../../../assets/images/teams/washington-wizards.svg'
+
+const Teams = [
+    {
+        'name':'atlanta-hawks',
+        'url': atlantaHawks
+    },
+    {
+        'name':'boston-celtics',
+        'url': bostonCeltics
+    },
+    {
+        'name':'brooklyn-nets',
+        'url': brooklynNets
+    },
+    {
+        'name':'charlotte-hornets',
+        'url': charlotteHornets
+    },
+    {
+        'name':'chicago-bulls',
+        'url': chicagoBulls
+    },
+    {
+        'name':'cleveland-cavaliers',
+        'url': clevelandCavaliers
+    },
+    {
+        'name':'dallas-mavericks',
+        'url': dallasMavericks
+    },
+    {
+        'name':'denver-nuggets',
+        'url': denverNuggets
+    },
+    {
+        'name':'detroit-pistons',
+        'url': detroitPiston
+    },
+    {
+        'name':'golden-state-warriors',
+        'url': goldenState
+    },
+    {
+        'name':'houston-rockets',
+        'url': houstonRockets
+    },
+    {
+        'name':'indiana-pacers',
+        'url': indianaPacers
+    },
+    {
+        'name':'los-angeles-clippers',
+        'url': losAngelesClippers
+    },
+    {
+        'name': 'los-angeles-lakers',
+        'url': losAngelesLakers
+    },
+    {
+        'name':'memphis-grizzlies',
+        'url': memphisGrizzlies
+    },
+    {
+        'name':'miami-heat',
+        'url': miamiHeat
+    },
+    {
+        'name':'milwaukee-bucks',
+        'url': milwaukeeBucks
+    },
+    {
+        'name':'minnesota-timberwolves',
+        'url': minnesotaTimberwolves
+    },
+    {
+        'name':'new-orleans-pelicans',
+        'url': newOrleansPelicans
+    },
+    {
+        'name':'new-york-knicks',
+        'url': newYorkKnicks
+    },
+    {
+        'name':'oklahoma-city-thunder',
+        'url': oklahomaCityThunder
+    },
+    {
+        'name':'orlando-magic',
+        'url': orlandoMagic
+    },
+    {
+        'name':'philadelphia-76ers',
+        'url': philadelphia76ers
+    },
+    {
+        'name':'phoenix-suns',
+        'url': phoenixSuns
+    },
+    {
+        'name':'portland-trail-blazers',
+        'url': portlandTrailBlazers
+    },
+    {
+        'name':'sacramento-kings',
+        'url': sacramentoKings
+    },
+    {
+        'name':'san-antonio-spurs',
+        'url': sanAntonioSpurs
+    },
+    {
+        'name':'toronto-raptors',
+        'url': torontoRaptors
+    },
+    {
+        'name':'utah-jazz',
+        'url': utahJazz
+    },
+    {
+        'name':'washington-wizard',
+        'url': washingtonWizards
+    },
+]
+
+export default Teams;
